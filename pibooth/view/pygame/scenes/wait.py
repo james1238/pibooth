616bff040802d022fa9fa450f504e210c0d21281
James Barnard <email.barnard@gmail.com>
2023-08-12 13:12:54 +0100

    no message

--- pibooth/view/pygame/scenes/wait.py
+++ pibooth/view/pygame/scenes/wait.py
@@ -35,7 +35,7 @@ class WaitScene(BasePygameScene):
 
     def resize(self, size):
         # Previous picture
-        self.image.set_rect(self.rect.centerx, 0, self.rect.width, self.rect.height)
+        self.image.set_rect(self.rect.centerx, 0 - self.rect.width, self.rect.width, self.rect.height)
         self.image_check.set_rect(*self.image.rect.inflate(-self.image.rect.width // 4,
                                                            -self.image.rect.width // 4))
 
